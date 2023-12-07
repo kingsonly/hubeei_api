819c351c250547948e41bd1d71b22d5c1b7c2f8e
kingsley achumie <kingsonly13c@gmail.com>
2023-12-07 12:46:17 +0100

    wip

--- database/migrations/2023_12_07_112704_add_engagement_type_to_engagement_table.php
+++ database/migrations/2023_12_07_112704_add_engagement_type_to_engagement_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('engagements', function (Blueprint $table) {
+        Schema::table('engagments', function (Blueprint $table) {
             $table->string("engagementType");
         });
     }
@@ -21,7 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('engagements', function (Blueprint $table) {
+
+        Schema::table('engagments', function (Blueprint $table) {
             $table->dropColumn("engagementType");
         });
     }
